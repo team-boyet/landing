3dc3f0ead0da9d5d5a10bd799f100bfcdf9b348b
Erg <59804530+aikoonchai@users.noreply.github.com>
2023-10-23 04:55:22 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import ThisNavbar from "./components/myNavbar";
 import About from "./components/About";
 import Platforms from './components/Platforms';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <ThisNavbar/>
+      <Header/>
+      <About/>
+      <Platforms/>
     </div>
   );
 }
