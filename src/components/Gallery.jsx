f727997a38ccae4af6153733690d5f9c98822136
Andrei-Cervantes <andrei29.cervantes@gmail.com>
2023-10-23 19:42:15 +0800

    Updated Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,248 @@
 import "./Gallery.css";
+import L1 from "../assets/gallery/L1.jpg";
+import L2 from "../assets/gallery/L2.jpg";
+import L3 from "../assets/gallery/L3.jpg";
+import L4 from "../assets/gallery/L4.jpg";
+import L5 from "../assets/gallery/L5.jpg";
+import L6 from "../assets/gallery/L6.jpg";
+import L7 from "../assets/gallery/L7.jpg";
+import L8 from "../assets/gallery/L8.jpg";
+import L9 from "../assets/gallery/L9.jpg";
+import L10 from "../assets/gallery/L10.jpg";
+import L11 from "../assets/gallery/L11.jpg";
+import L12 from "../assets/gallery/L12.jpg";
+import L13 from "../assets/gallery/L13.jpg";
+import L14 from "../assets/gallery/L14.jpg";
+import L15 from "../assets/gallery/L15.jpg";
+import L16 from "../assets/gallery/L16.jpg";
+import L17 from "../assets/gallery/L17.jpg";
+import L18 from "../assets/gallery/L18.jpg";
+import L19 from "../assets/gallery/L19.jpg";
+import L20 from "../assets/gallery/L20.jpg";
+import L21 from "../assets/gallery/L21.jpg";
+import L22 from "../assets/gallery/L22.jpg";
+import L23 from "../assets/gallery/L23.jpg";
+import L24 from "../assets/gallery/L24.jpg";
+import L25 from "../assets/gallery/L25.jpg";
+import L26 from "../assets/gallery/L26.jpg";
+import L27 from "../assets/gallery/L27.jpg";
+import L28 from "../assets/gallery/L28.jpg";
+import L29 from "../assets/gallery/L29.jpg";
+import L30 from "../assets/gallery/L30.jpg";
+import L31 from "../assets/gallery/L31.jpg";
+import L32 from "../assets/gallery/L32.jpg";
+import L33 from "../assets/gallery/L33.jpg";
+import L34 from "../assets/gallery/L34.jpg";
+import L35 from "../assets/gallery/L35.jpg";
+import L36 from "../assets/gallery/L36.jpg";
+import L37 from "../assets/gallery/L37.jpg";
+import L38 from "../assets/gallery/L38.jpg";
+import L39 from "../assets/gallery/L39.jpg";
+import L40 from "../assets/gallery/L40.jpg";
+import L41 from "../assets/gallery/L41.jpg";
+import L42 from "../assets/gallery/L42.jpg";
+import L43 from "../assets/gallery/L43.jpg";
+import L44 from "../assets/gallery/L44.jpg";
+import L45 from "../assets/gallery/L45.jpg";
+import L46 from "../assets/gallery/L46.jpg";
+import L47 from "../assets/gallery/L47.jpg";
+import L48 from "../assets/gallery/L48.jpg";
+import L49 from "../assets/gallery/L49.jpg";
+import L50 from "../assets/gallery/L50.jpg";
+import L51 from "../assets/gallery/L51.jpg";
+import L52 from "../assets/gallery/L52.jpg";
+import L53 from "../assets/gallery/L53.jpg";
+import L54 from "../assets/gallery/L54.jpg";
+import L55 from "../assets/gallery/L55.jpg";
+import L56 from "../assets/gallery/L56.jpg";
+import L57 from "../assets/gallery/L57.jpg";
+import L58 from "../assets/gallery/L58.jpg";
+import L59 from "../assets/gallery/L59.jpg";
+import L60 from "../assets/gallery/L60.jpg";
+import L61 from "../assets/gallery/L61.jpg";
+import L62 from "../assets/gallery/L62.jpg";
+import L63 from "../assets/gallery/L63.jpg";
+import L64 from "../assets/gallery/L64.jpg";
+import L65 from "../assets/gallery/L65.jpg";
+import L66 from "../assets/gallery/L66.jpg";
+import L67 from "../assets/gallery/L67.jpg";
+import L68 from "../assets/gallery/L68.jpg";
+import L69 from "../assets/gallery/L69.jpg";
+import L70 from "../assets/gallery/L70.jpg";
+import L71 from "../assets/gallery/L71.jpg";
+import L72 from "../assets/gallery/L72.jpg";
+import L73 from "../assets/gallery/L73.jpg";
+import L74 from "../assets/gallery/L74.jpg";
+import L75 from "../assets/gallery/L75.jpg";
+import L76 from "../assets/gallery/L76.jpg";
+import L77 from "../assets/gallery/L77.jpg";
+import L78 from "../assets/gallery/L78.jpg";
+import L79 from "../assets/gallery/L79.jpg";
+import L80 from "../assets/gallery/L80.jpg";
+import L81 from "../assets/gallery/L81.jpg";
+import L82 from "../assets/gallery/L82.jpg";
+import L83 from "../assets/gallery/L83.jpg";
+import L84 from "../assets/gallery/L84.jpg";
+import P1 from "../assets/gallery/P1.jpg";
+import P2 from "../assets/gallery/P2.jpg";
+import P3 from "../assets/gallery/P3.jpg";
+import P4 from "../assets/gallery/P4.jpg";
+import P5 from "../assets/gallery/P5.jpg";
+import P6 from "../assets/gallery/P6.jpg";
+import P7 from "../assets/gallery/P7.jpg";
+import P8 from "../assets/gallery/P8.jpg";
+import P9 from "../assets/gallery/P9.jpg";
+import P10 from "../assets/gallery/P10.jpg";
+import P11 from "../assets/gallery/P11.jpg";
+import P12 from "../assets/gallery/P12.jpg";
+import P13 from "../assets/gallery/P13.jpg";
+import P14 from "../assets/gallery/P14.jpg";
+import P15 from "../assets/gallery/P15.jpg";
 
 function Gallery() {
     return <>
         <div className="gallery-container">
             <h2>GALLERY</h2>
+            <div className="images-container">
+                <div className="L-row-images">
+                    <img src={L1} />
+                    <img src={L2} />
+                    <img src={L3} />
+                    <img src={L4} />
+                    <img src={L5} />
+                    <img src={L6} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L7} />
+                    <img src={L8} />
+                    <img src={L9} />
+                    <img src={L10} />
+                    <img src={L11} />
+                    <img src={L12} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L13} />
+                    <img src={L14} />
+                    <img src={L15} />
+                    <img src={L16} />
+                    <img src={L17} />
+                    <img src={L18} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L19} />
+                    <img src={L20} />
+                    <img src={L21} />
+                    <img src={L22} />
+                    <img src={L23} />
+                    <img src={L24} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L25} />
+                    <img src={L26} />
+                    <img src={L27} />
+                    <img src={L28} />
+                    <img src={L29} />
+                    <img src={L30} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L31} />
+                    <img src={L32} />
+                    <img src={L33} />
+                    <img src={L34} />
+                    <img src={L35} />
+                    <img src={L36} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L37} />
+                    <img src={L38} />
+                    <img src={L39} />
+                    <img src={L40} />
+                    <img src={L41} />
+                    <img src={L42} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L43} />
+                    <img src={L44} />
+                    <img src={L45} />
+                    <img src={L46} />
+                    <img src={L47} />
+                    <img src={L48} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L49} />
+                    <img src={L50} />
+                    <img src={L51} />
+                    <img src={L52} />
+                    <img src={L53} />
+                    <img src={L54} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L55} />
+                    <img src={L56} />
+                    <img src={L57} />
+                    <img src={L58} />
+                    <img src={L59} />
+                    <img src={L60} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L61} />
+                    <img src={L62} />
+                    <img src={L63} />
+                    <img src={L64} />
+                    <img src={L65} />
+                    <img src={L66} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L67} />
+                    <img src={L68} />
+                    <img src={L69} />
+                    <img src={L70} />
+                    <img src={L71} />
+                    <img src={L72} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L73} />
+                    <img src={L74} />
+                    <img src={L75} />
+                    <img src={L76} />
+                    <img src={L77} />
+                    <img src={L78} />
+                </div>
+                <div className="L-row-images">
+                    <img src={L79} />
+                    <img src={L80} />
+                    <img src={L81} />
+                    <img src={L82} />
+                    <img src={L83} />
+                    <img src={L84} />
+                </div>
+                <div className="P-row-images">
+                    <img src={P1} />
+                    <img src={P2} />
+                    <img src={P3} />
+                </div>
+                <div className="P-row-images">
+                    <img src={P4} />
+                    <img src={P5} />
+                    <img src={P6} />
+                </div>
+                <div className="P-row-images">
+                    <img src={P7} />
+                    <img src={P8} />
+                    <img src={P9} />
+                </div>
+                <div className="P-row-images">
+                    <img src={P10} />
+                    <img src={P11} />
+                    <img src={P12} />
+                </div>
+                <div className="P-row-images">
+                    <img src={P13} />
+                    <img src={P14} />
+                    <img src={P15} />
+                </div>
+            </div>
+            
         </div>
     </>
 }
